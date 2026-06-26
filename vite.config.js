1be66bc44d86987449e2c75f7429c4ef9e0a2131
Kenneth Foo <kennethaidanfoo@gmail.com>
2026-06-27 02:13:20 +0800

    Serve from custom domain signs.oddpaq.com (base:/ + CNAME)

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ base: '/signs/', plugins: [react()] })
+// Served from the ROOT of the custom domain https://signs.oddpaq.com/ , so base is '/'.
+// (If you ever drop the custom domain and serve from kafoosh.github.io/signs/, set this back to '/signs/'.)
+export default defineConfig({ base: '/', plugins: [react()] })
